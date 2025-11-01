562764719ef33dda0728a498e006473a333af6df
Constant <constantsuchet@gmail.com>
2025-11-01 09:02:40 +0100

    v1.00 SNAPSHOT

--- fix_textures.ps1
+++
@@ -1,2 +0,0 @@
-﻿Get-ChildItem -Filter naturescompass_*.json | ForEach-Object { $content = Get-Content $_.FullName -Raw; $content = $content -replace '"nature_compass/', '"item/nature_compass/'; Set-Content $_.FullName $content }
-
